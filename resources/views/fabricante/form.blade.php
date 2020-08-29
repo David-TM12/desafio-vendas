9bcc47d3014e5a06c49e3010090cad303b92c659
David Magalhães <deivi_gf144@hotmail.com>
2020-08-29 16:06:22 -0500

    criando tela de fabricantes

---
+++ resources/views/fabricante/form.blade.php
@@ -0,0 +1,17 @@
+@extends('adminlte::page')
+
+@section('title', 'Formulário de Fabricante')
+
+@section('content_header')
+    <h1>Fomulário de Fabricante</h1>    
+@stop
+
+@section('content')
+
+@stop
+
+@section('css')
+@stop
+
+@section('js')
+@stop
