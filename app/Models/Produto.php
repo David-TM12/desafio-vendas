9e869f8c424479db9c42bb96a36ca5b474f07fcb
David Magalhães <deivi_gf144@hotmail.com>
2020-09-23 20:59:26 -0400

    adicionando slugs dinamicos com cviebrock - flexpeak

--- app/Models/Produto.php
+++ app/Models/Produto.php
@@ -4,16 +4,34 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use PhpOffice\PhpSpreadsheet\Reader\Xls\Style\FillPattern;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class Produto extends Model
 {
+
+    use Sluggable;
 
     protected $fillable = [
         'descricao',
         'estoque',
         'preco',
-        'fabricante_id'
+        'slug',
+        'fabricante_id',
     ];
+
+     /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array 
+     */
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'descricao'
+            ]
+        ];
+    }
 
     public function fabricante()
     {   
